1f615b603725f1a9d92c67eac5ada9ba145c9b7e
Roshgar <rcourture@gmail.com>
2017-11-05 17:00:42 +0100

    fixed loginpage delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Platform } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import firebase from 'firebase';
@@ -11,6 +12,7 @@ import { LogInProvider } from '../providers/log-in/log-in';
 })
 export class MyApp {
   rootPage: any = LogInPage;
+  @ViewChild('myNav') nav: NavController;
 
   constructor(
     platform: Platform,
@@ -25,21 +27,28 @@ export class MyApp {
       if (!user) {
         console.log('redirect to login')
         self.rootPage = LogInPage;
-        self._loginProvider.setIsLogged(false);
+        self._loginProvider.setIsLogged(false)
+        self.nav.setRoot(LogInPage);
         //unsubscribe();
       } else {
         console.log('redirect to home');
         self.rootPage = HomePage;
         self._loginProvider.setIsLogged(true);
+        self.nav.setRoot(LogInPage);
         //unsubscribe();
       }
     });
     platform.ready().then(() => {
-
+      if (this._loginProvider.isLogged()) {
+        this.rootPage = HomePage;
+      }
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    if (this._loginProvider.isLogged()) {
+      this.rootPage = HomePage;
+    }
   }
 }
